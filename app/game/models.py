24a0535474be22d7a9bd23f9487ad4c3f2948353
asdyachkov <stepanderdichok@gmail.com>
2023-03-25 02:16:58 +0300

    created new models

--- app/game/models.py
+++ app/game/models.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
-from datetime import date
+from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import Text, Column, ForeignKey, Integer, DateTime
+from sqlalchemy import Text, Column, ForeignKey, Integer, DateTime, Boolean
 from sqlalchemy.orm import relationship, backref
 
 from app.store.database.sqlalchemy_base import db
@@ -11,45 +11,60 @@ from app.store.database.sqlalchemy_base import db
 @dataclass
 class GameDC:
     chat_id: int
-    players: list["PlayerDC"]
-    created_at: Optional[DateTime] = date.today()
+    is_start: bool
+    is_end: bool
+    round_id: int
+    created_at: Optional[DateTime] = datetime.now()
 
 
 @dataclass
 class PlayerDC:
     vk_id: int
+    is_admin: bool
     name: str
     last_name: str
-    score: "GameScoreDC"
+    photo_id: str
+    state: int
+    round_id: int
+    score: int = 0
 
 
 @dataclass
-class GameScoreDC:
-    points: int
+class RoundDC:
+    state: int
+    players: list["PlayerDC"]
+    game_id: int
 
 
 class GameDCModel(db):
     __tablename__ = "games"
+    id = Column(Integer(), primary_key=True)
+    chat_id = Column(Integer(), nullable=False)
+    is_start = Column(Boolean(), default=False, nullable=False)
+    is_end = Column(Boolean(), default=False, nullable=False)
     created_at = Column(DateTime(), nullable=False)
-    chat_id = Column(Integer(), nullable=False, primary_key=True)
-    players = relationship("PlayerDCModel", back_populates="games")
+    rounds = relationship("RoundDCModel", backref=backref("games", uselist=False, cascade="all, delete-orphan"))
+
+
+class RoundDCModel(db):
+    __tablename__ = "rounds"
+    id = Column(Integer(), primary_key=True)
+    state = Column(Integer(), nullable=False, default=0)
+    game_id = Column(Integer(), ForeignKey("games.id"))
+    players = relationship("PlayerDCModel", backref=backref("rounds", uselist=True, cascade="all, delete-orphan"))
 
 
 class PlayerDCModel(db):
     __tablename__ = "players"
-    vk_id = Column(Integer(), primary_key=True)
+    id = Column(Integer(), primary_key=True)
+    vk_id = Column(Integer(), nullable=False)
+    is_admin = Column(Boolean(), default=False, nullable=False)
     name = Column(Text(), nullable=False)
     last_name = Column(Text(), nullable=False)
-    game_id = Column(ForeignKey("games.chat_id"))
-    games = relationship("GameDCModel", back_populates="players")
-    score_id = Column(ForeignKey("scores.id"))
-    scores = relationship(
-        "GameScoreDCModel",
-        backref=backref("players", uselist=False, cascade="all, delete-orphan"),
-    )
+    photo_id = Column(Text(), nullable=False)
+    score = Column(Integer(), nullable=False, default=0)
+    state = Column(Integer(), nullable=False, default=0)
+    round_id = Column(Integer(), ForeignKey("rounds.id"))
 
 
-class GameScoreDCModel(db):
-    __tablename__ = "scores"
-    id = Column(Integer(), primary_key=True)
-    points = Column(Integer(), unique=True)
+
