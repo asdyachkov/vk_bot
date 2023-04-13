c0bd8775883dc89dbf0a3cab7a6152912411eed4
Stepan <stepanderdichok@gmail.com>
2023-04-13 23:34:00 +0300

    upgrade allowed django hosts

--- docker_admin/vk_bot_admin/settings.py
+++ docker_admin/vk_bot_admin/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', '0.0.0.0']
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', '0.0.0.0', '95.163.241.174']
 
 
 # Application definition
